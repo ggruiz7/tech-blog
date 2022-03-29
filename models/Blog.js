62220df32160a441c660b8cbe6bf0e3aee14b393
Gabriel Ruizz <garciagabriel1945@gmail.com>
2022-03-28 19:47:13 -0700

    blog model added

---
+++ models/Blog.js
@@ -0,0 +1,36 @@
+class Post extends Model {}
+
+// subject to change
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+    },
+    body: {
+      type: DataTypes.STRING,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
